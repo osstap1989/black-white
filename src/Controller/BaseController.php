df4eb3182bd3be94eb5d10cbf5c37f824f45394f
SofiiaMaiak <mayaksofia1997@gmail.com>
2018-06-16 13:44:52 +0300

    Initial commit

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class BaseController extends Controller
 {
     /**
-     * @Route("/index", name="index")
+     * @Route("/index.html", name="index.html")
      */
     public function index()
     {
@@ -17,12 +17,42 @@ class BaseController extends Controller
         ]);
     }
     /**
-     * @Route("/contacts", name="contacts")
+     * @Route("/contacts.html", name="contacts.html")
      */
 
     public function contacts()
     {
         return $this->render('contacts.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+     /**
+     * @Route("/about.html", name="about.html")
+     */
+
+    public function about()
+    {
+        return $this->render('about.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+     /**
+     * @Route("/work.html", name="work.html")
+     */
+
+    public function work()
+    {
+        return $this->render('work.html.twig', [
+            'controller_name' => 'BaseController',
+        ]);
+    }
+     /**
+     * @Route("/blog.html", name="blog.html")
+     */
+
+    public function blog()
+    {
+        return $this->render('blog.html.twig', [
             'controller_name' => 'BaseController',
         ]);
     }
